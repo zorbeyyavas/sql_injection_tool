69162dbbff3bdb4d09706f1b8fc64e3235f0d285
zorbeyyavas <yavaszorbey@gmail.com>
2024-11-05 19:55:54 +0300

    Update main.py

--- sqli_tool/main.py
+++ sqli_tool/main.py
@@ -14,20 +14,25 @@ def main():
     parser.add_argument('-c', '--config', type=str, help='Konfigürasyon dosyası')
     args = parser.parse_args()
 
+    # Konfigürasyon dosyasını yükle
     config = load_config(args.config) if args.config else {}
-    
+
+    # Payload, hata mesajları ve kullanıcı ajanları dosya yollarını al
     payload_file_path = config.get('payloads_file', 'sorgu.txt')
     error_file_path = config.get('errors_file', 'errors.txt')
-    user_agent_file_path = 'user-agents.txt'
+    user_agent_file_path = config.get('user_agents_file', 'user-agents.txt')
 
+    # Payload'ları, hata mesajlarını ve kullanıcı ajanlarını yükle
     payloads = load_payloads(payload_file_path)
     error_messages = load_error_messages(error_file_path)
     user_agents = load_user_agents(user_agent_file_path)
 
+    # Gerekli dosyalar yüklenemediyse hata mesajı göster
     if not payloads or not error_messages or not user_agents:
         logging.error("Gerekli dosyalar yüklenemedi, işlem yapılamıyor.")
         return
 
+    # POST verilerini ayrıştır
     post_data = {}
     if args.data:
         try:
@@ -36,6 +41,7 @@ def main():
             logging.error("POST verileri hatalı formatta.")
             return
 
+    # SQL enjeksiyon testini başlat
     test_sql_injection(args.url, payloads, error_messages, user_agents, is_post=args.post, data=post_data)
 
 if __name__ == "__main__":
